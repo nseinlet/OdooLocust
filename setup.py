e69d2ed36cbba874c559be0d5ec7916b06ee9352
Nicolas Seinlet <nse@odoo.com>
2023-11-03 10:31:02 +0100

    [IMP] bump version so 1.6.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@
 from setuptools import setup, find_packages
 
 setup(name='OdooLocust',
-      version='1.6.2',
+      version='1.6.3',
       description='Easily load test Odoo using Locust and odoolib.',
       author='Nicolas Seinlet',
       author_email='',
