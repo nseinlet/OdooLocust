3958f72e7dd7bea5bab9e07a350a892a7c887112
Nicolas Seinlet <nicolas@seinlet.com>
2025-04-23 07:54:00 +0200

    WIP

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@
 from setuptools import setup, find_packages
 
 setup(name='OdooLocust',
-      version='1.6.8',
+      version='1.6.9',
       description='Easily load test Odoo using Locust and odoolib.',
       author='Nicolas Seinlet',
       author_email='',
